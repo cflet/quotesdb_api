9b61670b8202d08383ff491726710b672d418daf
Cheron Fletcher <127625161+cflet@users.noreply.github.com>
2023-03-18 16:41:08 -0500

    Add Categories

---
+++ api/categories/read.php
@@ -0,0 +1,49 @@
+<?php
+
+//Headers
+header('Access-Control-Allow-Origin: *' );
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+//Instantiate DB & Connect
+$database = new Database();
+$db = $database->connect();
+
+//Instantiate Blog Category object
+$category = new Category($db);
+
+//Call blog read method
+$result = $category->read();
+//Get row count
+$num = $result->rowCount();
+
+//Check for any post
+if($num > 0) {
+    //Post array
+     $categories_arr = array();
+     $categories_arr['data'] = array();
+
+     while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+    
+        $category_item = array(
+            'id' => $id,
+            'category' => $category
+        );
+    
+        //Push to "data"
+        array_push($categories_arr['data'], $category_item);
+     }
+
+      //Turn to JSON & output
+    echo json_encode($categories_arr);
+
+} else {
+    //No Categories
+    echo json_encode(
+       array('message' => 'No Categories Found')
+    );
+   }
+   
