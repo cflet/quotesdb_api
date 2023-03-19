16a86f9c05a912cd272603321762683c84e1b244
Cheron Fletcher <127625161+cflet@users.noreply.github.com>
2023-03-19 09:11:43 -0500

    Update authors index

--- api/authors/index.php
+++ api/authors/index.php
@@ -8,6 +8,7 @@ include_once '../../config/Database.php';
 include_once '../../models/Author.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
+$args = $_SERVER['QUERY_STRING'];
 
 
 if ($method === 'OPTIONS') {
@@ -19,6 +20,6 @@ if ($method === 'OPTIONS') {
 
 if($method == 'PUT') include 'update.php';
 elseif($method == 'POST') include 'create.php';
-elseif($method == 'GET') include 'read.php';
+elseif($method == 'GET' && $args == null) include 'read.php';
 elseif($method == 'DELETE') include 'delete.php';
-elseif($_SERVER['QUERY_STRING'] != null) include 'read_single.php';
+elseif($args) include 'read_single.php';
